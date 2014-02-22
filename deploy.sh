73f95ec4d37f8723e8fb12354ad03f325afbdab6
John Montgomery <johnbmontgomery@gmail.com>
2014-02-22 08:19:45 -0500

    Added basic deployment script.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#Add my bash file to the end of the system one.
+#Sometimes there's important things in whatever the system has made.
+echo "source ~/dotfiles/.bash_profile" >> ~/.bash_profile
+
+cp -i ~/dotfiles/.vimrc ~/.vimrc
+cp -i ~/dotfiles/.vim ~/.vim
+cp -i ~/dotfiles/.tmux.conf ~/.tmux.conf
+cp -i ~/dotfiles/.git-completion.bash ~/.git-completion.bash
