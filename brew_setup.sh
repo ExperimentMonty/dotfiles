9d313358b214e033b0689212b9fc75d89f3e875e
John Montgomery <johnbmontgomery@gmail.com>
2019-09-12 15:41:16 -0400

    Add brew files for OSX Homebrew setup

---
+++ brew_setup.sh
@@ -0,0 +1,13 @@
+# This assumes that you've already installed brew on your system.
+brew update
+brew upgrade
+
+brew install bash-completion
+brew install brew-cask-completion
+brew install curl
+brew install dos2unix
+brew install python
+brew install thefuck
+brew install tmux
+brew install wget
+brew install zsh
