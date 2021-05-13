0a42e80492232ce11c498ffe72e4aa1c41e67005
John Montgomery <john.montgomery@affirm.com>
2021-05-13 11:58:51 -0400

    Stop using bash things in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #Add my bash file to the end of the system one.
 #Sometimes there's important things in whatever the system has made.
-echo "source ~/dotfiles/.bash_profile" >> ~/.bash_profile
+# I need to rework this because I use zsh now.
+# Probably want to add a step to install oh-my-zsh as well
+#echo "source ~/dotfiles/.bash_profile" >> ~/.bash_profile
 
 cp -i ~/dotfiles/.vimrc ~/.vimrc
 cp -i -r ~/dotfiles/.vim ~/.vim
